4d5719806b42750945cc84c8b2bfe0436ec63cb9
Gheorghii28 <popovici.gheorghii89@gmail.com>
2024-04-08 22:59:04 +0200

    Add Google Fonts stylesheet for Material Symbols Outlined and update icon

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { UserProfile } from '../../models/user-profile.model';
@@ -8,7 +7,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, FormsModule, CommonModule],
+  imports: [MatButtonModule, FormsModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
