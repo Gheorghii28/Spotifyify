cf0eb16ae75b5997a80890473322543ed0dc92f1
Gheorghii28 <popovici.gheorghii89@gmail.com>
2024-04-19 22:40:42 +0200

    Adjusted routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,20 @@ import { LoginComponent } from './views/login/login.component';
 import { LayoutComponent } from './layout/layout.component';
 import { SongsComponent } from './views/songs/songs.component';
 import { authGuard } from './guards/auth.guard';
+import { HomeComponent } from './views/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
   {
     path: '',
     component: LayoutComponent,
     canActivate: [authGuard],
     children: [
+      { path: 'home', component: HomeComponent },
       { path: 'songs', component: SongsComponent },
     ],
   },
