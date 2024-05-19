fb149edbe4c4851e9fcb0cfc788278cc8851ef79
Gheorghii28 <popovici.gheorghii89@gmail.com>
2024-05-19 20:12:09 +0200

    refactor: Update SpotifyService to handle different response types and added fetchLikedStatusForTrack method to fetch liked status for a track.

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -7,7 +7,11 @@ import { TokenService } from './token.service';
 export class SpotifyService {
   constructor(private tokenService: TokenService) {}
 
-  async fetchWebApi(endpoint: string, method: string) {
+  async fetchWebApi(
+    endpoint: string,
+    method: string,
+    responseType: 'json' | 'text'
+  ) {
     const token = this.tokenService.getAccessToken();
     const res = await fetch(`https://api.spotify.com/${endpoint}`, {
       headers: {
@@ -15,15 +19,29 @@ export class SpotifyService {
       },
       method,
     });
-    return await res.json();
+    if (responseType === 'json') {
+      return await res.json();
+    } else {
+      return await res.text();
+    }
   }
 
-  async getSpotifyData(endpoint:string) {
-    return (
-      await this.fetchWebApi(
-        `v1/${endpoint}`,
-        'GET'
-      )
+  async getSpotifyData(endpoint: string) {
+    return await this.fetchWebApi(`v1/${endpoint}`, 'GET', 'json');
+  }
+
+  async updateSpotifyData(endpoint: string) {
+    return await this.fetchWebApi(`v1/${endpoint}`, 'PUT', 'text');
+  }
+
+  async removeSpotifyData(endpoint: string) {
+    return await this.fetchWebApi(`v1/${endpoint}`, 'DELETE', 'text');
+  }
+
+  async fetchLikedStatusForTrack(id: string) {
+    const likedStatusArr = await this.getSpotifyData(
+      `me/tracks/contains?ids=${id}`
     );
+    return likedStatusArr[0];
   }
 }
